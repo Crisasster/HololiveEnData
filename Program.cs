4c6de7d34c2c823c196db4c23a2d83c4911e73a6
Cristina McClain <cristinarmcclain@gmail.com>
2023-07-25 15:02:01 -0400

    source code set up

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
 
 namespace HoloEnglishData
 {
@@ -10,6 +11,60 @@ namespace HoloEnglishData
     {
         static void Main(string[] args)
         {
+            SQLiteConnection sqliteConnection;
+            sqliteConnection = CreateConnection();
+            CreateTable(sqliteConnection);
+            InsertData(sqliteConnection);
+            ReadData(sqliteConnection);
+        }
+
+        //set up connection
+        static SQLiteConnection CreateConnection()
+        {
+            SQLiteConnection sqliteConn;
+            sqliteConn = new SQLiteConnection("Data Source=C:\\Users\\goper\\source\\repos\\HololiveQuery\\NewFolder1\\Databased\\HololiveQuery.db; Version = 3; New = True; Compress = True;");
+            try
+            {
+                sqliteConn.Open();
+            }
+            catch
+            {
+
+            }
+            return sqliteConn;
+        }
+        //Create table
+        static void CreateTable(SQLiteConnection conn)
+        {
+            SQLiteCommand sqliteCommand;
+            string createSQL = "CREATE TABLE Members(Name VARCHAR(100), Channel VARCHAR(100), Description VARCHAR(100))";
+            sqliteCommand = conn.CreateCommand();
+            sqliteCommand.CommandText = createSQL;
+            sqliteCommand.ExecuteNonQuery();
+        }
+        //Insert Values
+        static void InsertData(SQLiteConnection conn)
+        {
+            SQLiteCommand sqliteCommand;
+            sqliteCommand = conn.CreateCommand();
+            sqliteCommand.CommandText = "INSERT INTO Members(Name, Channel, Description) VALUES ('Mori Calliope', 'https://www.youtube.com/@MoriCalliope', 'Grim Reaper Idol of HoloEN! Love making music!!');";
+            sqliteCommand.ExecuteNonQuery();
+        }
+
+        //Read data
+        static void ReadData(SQLiteConnection conn)
+        {
+            SQLiteDataReader sqliteReader;
+            SQLiteCommand sqliteCommand;
+            sqliteCommand = conn.CreateCommand();
+            sqliteCommand.CommandText = "SELECT * FROM Members";
+            sqliteReader = sqliteCommand.ExecuteReader();
+            while (sqliteReader.Read())
+            {
+                string readerString = sqliteReader.GetString(0);
+                Console.WriteLine(readerString);
+            }
+            conn.Close();
         }
     }
 }
