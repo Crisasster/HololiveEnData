6f70e7f83704162275e7b412087381c93753be27
Cristina McClain <cristinarmcclain@gmail.com>
2023-07-26 14:56:51 -0400

    updated files

--- Program.cs
+++ Program.cs
@@ -9,8 +9,24 @@ namespace HoloEnglishData
 {
     internal class Program
     {
+        class Members
+        {
+            public class Council
+            {
+                public int id;
+                public string Name;
+                public string Channel;
+                public string Description;
+            }
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello. Virtual Youtubers are streamers who use virtual avatars.");
+            Console.WriteLine("You may have heard of Kizuna Ai, she is known to be the first vtuber.");
+            Console.WriteLine("Hololive is currently the power house of vtubers.");
+            Console.WriteLine("Besides their Japanese branch, they also have English and Indonesian Branches");
+
+            //create connection
             SQLiteConnection sqliteConnection;
             sqliteConnection = CreateConnection();
             CreateTable(sqliteConnection);
@@ -18,7 +34,7 @@ namespace HoloEnglishData
             ReadData(sqliteConnection);
         }
 
-        //set up connection
+        //set up connection to database
         static SQLiteConnection CreateConnection()
         {
             SQLiteConnection sqliteConn;
@@ -42,16 +58,21 @@ namespace HoloEnglishData
             sqliteCommand.CommandText = createSQL;
             sqliteCommand.ExecuteNonQuery();
         }
+
         //Insert Values
         static void InsertData(SQLiteConnection conn)
         {
             SQLiteCommand sqliteCommand;
             sqliteCommand = conn.CreateCommand();
             sqliteCommand.CommandText = "INSERT INTO Members(Name, Channel, Description) VALUES ('Mori Calliope', 'https://www.youtube.com/@MoriCalliope', 'Grim Reaper Idol of HoloEN! Love making music!!');";
+            sqliteCommand.ExecuteNonQuery();
             sqliteCommand.CommandText = "INSERT INTO Members(Name, Channel, Description) VALUES ('Takanashi Kiara', 'https://www.youtube.com/@TakanashiKiara', 'An idol whose dream is to become a fast food shop owner. She is a phoenix NOT a chicken or turkey. Very important');";
+            sqliteCommand.ExecuteNonQuery();
             sqliteCommand.CommandText = "INSERT INTO Members(Name, Channel, Description) VALUES ('Ninomae Inanis', 'https://www.youtube.com/@NinomaeInanis', 'She began her VTuber activities to deliver random sanity checks on humanity as an ordinary girl.');";
+            sqliteCommand.ExecuteNonQuery();
             sqliteCommand.CommandText = "INSERT INTO Members(Name, Channel, Description) VALUES ('Watson Amelia', 'https://www.youtube.com/@WatsonAmelia', 'Weekend Streams ~ Detective. Gamer. Idol? Ame!');";
-            sqliteCommand.CommandText = "INSERT INTO Members(Name, Channel, Description) VALUES ('Gawr Gura', 'https://www.youtube.com/@GawrGura', 'Shark-girl Idol of Hololive EN !);";
+            sqliteCommand.ExecuteNonQuery();
+            sqliteCommand.CommandText = "INSERT INTO Members(Name, Channel, Description) VALUES ('Gawr Gura', 'https://www.youtube.com/@GawrGura', 'Shark-girl Idol of Hololive EN!');";
             sqliteCommand.ExecuteNonQuery();
         }
 
